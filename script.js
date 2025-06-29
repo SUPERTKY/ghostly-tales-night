62606ffd676506bbb53fd41b8f8b3a86e132ce12
SUPERTKY <purple_chips@outlook.jp>
2025-06-29 17:16:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-let activated = false;
+const bgm = document.getElementById("bgm");
+const playButton = document.getElementById("playButton");
 
-document.body.addEventListener("click", () => {
-  if (activated) return;
-  activated = true;
-
-  const bgm = document.getElementById("bgm");
-  const playButton = document.getElementById("playButton");
-
+function activateGame() {
   // BGM再生
   bgm.play().catch((e) => console.error("BGM再生失敗:", e));
 
   // ボタン有効化
   playButton.classList.add("enabled");
 
-  // ボタンクリック処理
-  playButton.addEventListener("click", () => {
-    alert("ゲームスタート！");
-    // location.href = "game.html";
-  });
+  // 画面全体クリック判定を削除
+  document.body.removeEventListener("click", activateGame);
+}
+
+// 最初の一回だけ有効なクリックイベント
+document.body.addEventListener("click", activateGame);
+
+// 遊ぶボタンクリック処理
+playButton.addEventListener("click", () => {
+  alert("ゲームスタート！");
+  // location.href = "game.html";
 });
