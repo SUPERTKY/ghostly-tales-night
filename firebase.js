bd0787442f371a4270ce423b93e18ccae072c4f9
SUPERTKY <purple_chips@outlook.jp>
2025-06-29 20:43:07 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,11 +2,12 @@
 const firebaseConfig = {
   apiKey: "AIzaSyB1hyrktLnx7lzW2jf4ZeIzTrBEY-IEgPo",
   authDomain: "horror-game-9b2d2.firebaseapp.com",
+  databaseURL: "https://horror-game-9b2d2-default-rtdb.asia-southeast1.firebasedatabase.app", // ✅新しい正しいURL
   projectId: "horror-game-9b2d2",
   storageBucket: "horror-game-9b2d2.firebasestorage.app",
   messagingSenderId: "534762448588",
   appId: "1:534762448588:web:e0a6a01cd14c7f1dce4469"
-  databaseURL: "https://horror-game-9b2d2-default-rtdb.asia-southeast1.firebasedatabase.app", // ✅新しい正しいURL
+  
 };
 
 // 初期化
