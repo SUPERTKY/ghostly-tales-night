449da1485386738441ca7c6586970d641217e2b2
SUPERTKY <purple_chips@outlook.jp>
2025-06-29 20:19:36 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,14 @@
-// あなたのFirebase設定情報に置き換えてください（プロジェクト設定から取得）
+
 const firebaseConfig = {
-  apiKey: "XXXXX",
-  authDomain: "XXXXX.firebaseapp.com",
-  databaseURL: "https://XXXXX.firebaseio.com",
-  projectId: "XXXXX",
-  storageBucket: "XXXXX.appspot.com",
-  messagingSenderId: "XXXXX",
-  appId: "XXXXX"
+  apiKey: "AIzaSyB1hyrktLnx7lzW2jf4ZeIzTrBEY-IEgPo",
+  authDomain: "horror-game-9b2d2.firebaseapp.com",
+  projectId: "horror-game-9b2d2",
+  storageBucket: "horror-game-9b2d2.firebasestorage.app",
+  messagingSenderId: "534762448588",
+  appId: "1:534762448588:web:e0a6a01cd14c7f1dce4469"
 };
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
